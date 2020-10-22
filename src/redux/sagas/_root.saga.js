b2752ab807422d7b11e93218bb447f881f2a0ded
Jeff McMahon <jeffmcmahon1973@gmail.com>
2020-10-22 16:32:36 -0500

    items saga and root saga update

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -1,8 +1,9 @@
 import { all } from 'redux-saga/effects';
+import itemsSaga from './item.saga';
 import loginSaga from './login.saga';
 import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
-
+import itemsSaga from './items.saga';
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
 // This is imported in index.js as rootSaga
@@ -15,5 +16,6 @@ export default function* rootSaga() {
     loginSaga(), // login saga is now registered
     registrationSaga(),
     userSaga(),
+    itemsSaga(),
   ]);
 }
