678729b141ca0f3a0818c7e23169ec5cd2cec29a
Jeff McMahon <jeffmcmahon1973@gmail.com>
2020-10-22 16:36:24 -0500

    bug fixes - sagas work

--- src/redux/sagas/items.saga.js
+++ src/redux/sagas/items.saga.js
@@ -8,7 +8,7 @@ function* fetchItems() {
       withCredentials: true,
     };
 
-    const response = yield axios.get('api/shelf', config);
+    const response = yield axios.get('/api/shelf', config);
 
     yield put({ type: 'SET_ITEMS', payload: response.data });
   } catch (error) {
