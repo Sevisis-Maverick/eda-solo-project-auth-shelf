43c9618ca31053ef4b3e3757b437e9e4d82fe5b6
Jeff McMahon <jeffmcmahon1973@gmail.com>
2020-10-23 10:05:46 -0500

    info page ready for merge

--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-
-// This is one of our simplest components
+import { connect } from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
+/* import Delete from '../Delete/Delete';
+ */// This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
@@ -12,15 +14,36 @@ import React, {Component} from 'react';
 
 class InfoPage extends Component {
 
+  componentDidMount() {
+    this.getItems();
+  }
 
-
+  getItems = () => {
+    console.log('Getting items');
+    this.props.dispatch({
+      type: 'SET_ITEMS'
+    })
+  };
+  
+  
   render() {
     return (
       <div>
-        <p>Info Page</p>
+      <p>Info Page</p>
+      <ul>
+        {this.props.reduxStore.itemsReducer.map((item, id))}
+        <img alt ={item.id} src={item.url} />
+        <p>{item.description}</p>
+
+{/*         <Delete />
+ */}      </ul>
       </div>
     )
   }
 }
 
-export default InfoPage;
+const mapReduxStateToProps = (reduxState) => ({
+  reduxState
+});
+
+export default connect(mapReduxStateToProps)(InfoPage);
